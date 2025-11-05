15823d50c92d93c2a8283189a4276f1d97f49c9d
WildChangancheng <changanchengmsqk@outlook.com>
2025-11-05 21:45:13 +0800

    final version of C writen quick sort

--- BasicAlgorithm/src/sort/quick/quick.c
+++ BasicAlgorithm/src/sort/quick/quick.c
@@ -10,20 +10,42 @@ void change(int* lst, int ele1_index, int ele2_index) {
     return ;
 }
 
+int process(int *lst, int low, int high) {
+    int pivot_key = lst[low];
 
-typedef struct array
-{
-    
-};
+    while (low < high){
+        while ((low < high) && pivot_key < lst[high]) high --;
+        change(lst, high, low);
 
+        while ((low < high) && pivot_key > lst[low]) low ++;
+        change(lst, high, low);
+    }
+    return low;
+}
 
-int main()
-{
-    int arr[5];
-    arr[0] = 1;
-    arr[1] = 2;
+void quick_sort(int *lst, int length) {
+    int m;
+    m = process(lst, 0, length-1);
+    process(lst, 0, m-1);
+    process(lst, m+1, length-1);
+    return ;
+}
 
-    change(arr, 0, 1);
-    printf("%d , %d\n", arr[0], arr[1]);
+void print_array(int *arr, int n, const char *msg) {
+    printf("%s: ", msg);
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 3, 9, 1, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    print_array(arr, n, "Before sorting:");
+
+    quick_sort(arr, n);
+
+    print_array(arr, n, "Sorted:");
+
     return 0;
 }
